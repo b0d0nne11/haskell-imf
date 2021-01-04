50f8ba5eafa829be9c3db0ef5c66888515973ada
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2021-01-04 00:05:17 -0600

    Drop dep on base36 library

--- imf/src/Data/IMF/MessageId.hs
+++ imf/src/Data/IMF/MessageId.hs
@@ -6,14 +6,23 @@ module Data.IMF.MessageId
   )
 where
 
+import           Data.Char             (chr, ord)
 import           Data.Text             (Text)
 import qualified Data.Text             as T
-import           Data.Text.Base36      (encode)
 import           Data.Time.Clock.POSIX (getPOSIXTime)
 import           Data.Word             (Word32)
+import           Numeric               (showIntAtBase)
 import           System.Random         (randomIO)
 
 import           Data.IMF.Types
+
+base36 :: (Integral a, Show a) => a -> Text
+base36 a = T.pack $ showIntAtBase 36 intToDigit a ""
+  where
+    intToDigit i
+      | i >= 0  && i <=  9 = chr $ ord '0' + i
+      | i >= 10 && i <= 35 = chr $ ord 'a' + i - 10
+      | otherwise = error $ "invalid base 36 digit: " ++ show i
 
 data MessageId = MessageId Text Text
   deriving (Show, Eq)
@@ -28,5 +37,4 @@ newMessageId :: Text -> IO MessageId
 newMessageId domain = do
     t <- truncate . (1e6 *) <$> getPOSIXTime
     r <- randomIO :: IO Word32
-    return $ MessageId (encode t <> "." <> encode r) domain
-
+    return $ MessageId (base36 t <> "." <> base36 r) domain
